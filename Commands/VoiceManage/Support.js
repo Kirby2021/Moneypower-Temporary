76a62ef0ecac7e78762ad8b20a02d9c8ab499990
Fan. as Fandy <75095826+FandyAnon@users.noreply.github.com>
2021-08-06 17:22:06 +0800

    ✅

---
+++ Commands/VoiceManage/Support.js
@@ -0,0 +1,31 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	name: 'Help',
+	description: 'List all of my commands or info about a specific command.',
+  guildOnly: false,
+  myServerOnly: false,
+	aliases: ['a'],
+	usage: `help <command_name>`,
+	cooldown: 5,
+	run: async (client, message, args) => {
+     let helpEmbed1 = new MessageEmbed()
+
+		.setAuthor(client.user.username)
+    .setDescription("Theres problem? contact us")
+    .setThumbnail(client.user.displayAvatarURL())
+    .setFooter("Support by Fan#7108")
+    .addFields(
+                
+                {
+                    name: "SUPPORT SERVER",
+                    value: "**DISCORD.GG/MONEYPOWER**",
+                    inline: false
+                }
+           
+    )
+    .setColor("BLACK")
+    .setFooter(`${client.user.username} Created by Fan#7108 with Logic`)
+    message.inlineReply(helpEmbed1)
+    }
+  }
