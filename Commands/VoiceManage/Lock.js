e044dd0951dbde6f5458cb78ad81fc8f2dc7f92c
Fan. as Fandy <75095826+FandyAnon@users.noreply.github.com>
2021-08-06 17:04:56 +0800

    ✅

--- Commands/VoiceManage/Lock.js
+++ Commands/VoiceManage/Lock.js
@@ -1,1 +1,49 @@
 
+const { MessageEmbed } = require("discord.js");
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "Lock",
+  aliases: ["lock",'locked'],
+  description: "Set the limit as the user in voice",
+  usage: "Lock",
+  ownerOnly: false,
+  myServerOnly: false,
+  guildOnly: false,
+  cooldown: 2,
+	run: async (client, message, args) => {
+
+let channel = message.member.voice.channel; 
+if (!channel)return message.channel.send({
+                        embed: {
+                            color: "RED",
+                            description :`Setting channel on your own channel`
+                        }
+   
+                   })
+if (!message.member.permissionsIn(channel).has('MANAGE_CHANNELS'))
+  return message.inlineReply('⚠ - The channel is owned');
+
+if (!message.guild.me.permissionsIn(channel).has('MANAGE_CHANNELS'))
+  return message.inlineReply('⚠ - I dont have authority to manage'); 
+       
+  if (message.member.voice.channel) {
+  
+  let ika = new Discord.MessageEmbed()
+  
+  .setTitle("Channel Edited")
+  .setDescription(`\`🔒\` Channel Lock the limit by **${message.author.username}** as the user in voice`)
+  .setFooter(`© ${message.guild.name} 2021`)
+  .setColor("GREEN")
+  
+  message.inlineReply(ika)     
+  let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
+  for (const [memberID, member] of channel.members) {
+    
+    
+    message.member.voice.channel.setUserLimit(channel.members.size);  
+  }
+} 
+}
+}
+  
